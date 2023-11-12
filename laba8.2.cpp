19905859f10e0b1be10d3daed342c384f0260faf
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2023-11-12 18:20:17 +0200

    Add files via upload

---
+++ laba8.2.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+#include <fstream>
+#define N 3000
+#define SYMBS 33
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::ifstream fin("text.txt");
+
+    char buff[100];
+
+    for (int i = 0; i < 100; i++)
+        buff[i] = 0;
+
+    char words[N][100];
+    int k = 0;
+
+    while (!fin.eof())      // read
+    {
+        fin >> buff;
+
+        for (int i = 0; i < 100; i++)
+        {
+            words[k][i] = buff[i];
+            k++;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            buff[i] = 0;
+        }
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            if (words[i][j] >= 'а' && words[i][j] <= 'я')     // меняем регистр
+                words[i][j] -= 'а' - 'А';
+        }
+    }
+
+    int symbs[N][100];    
+
+    for (int i = 0; i < SYMBS; i++)
+        for (int j = 0; j < 100; j++)                     // очищаем массив
+            symbs[i][j] = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            if (words[i][j] >= 'а' && words[i][j] <= 'я') {    // здесь я хочу записать слово в виде цифр на основе анси
+                symbs[i][j] = (int)words[i][j];                // если это вообще нужно
+            }
+
+            // здесь узнать количество букв в слове
+            // потом как-то перевернуть слово и сравнить его с изначальным
+        }
+
+    }
+}
+
