5ae4ea97311826fb3572dc500f800e792618b0ad
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2023-11-13 21:01:30 +0200

    Update laba8.2.cpp

--- laba8.2.cpp
+++ laba8.2.cpp
@@ -39,19 +39,13 @@ int main()
     {
         for (int j = 0; j < 100; j++)
         {
-            if (words[i][j] >= 'а' && words[i][j] <= 'я')     // меняем регистр
+            if (words[i][j] >= 'а' && words[i][j] <= 'я')     // меняем регистр на строчные буквы
                 words[i][j] -= 'а' - 'А';
         }
     }
 
-    /*int symbs[N][100];
-
-    for (int i = 0; i < SYMBS; i++)
-        for (int j = 0; j < 100; j++)                     // очищаем массив
-            symbs[i][j] = 0;
-            */
-
     int kol = 0;   // количество букв в слове
+    bool flag = false;
 
     for (int i = 0; i < k; i++)
     {
@@ -65,7 +59,7 @@ int main()
 
         int m = 0;    //счетчик
         int n = kol;    //изменяемое количество букв
-        bool flag = false;
+        
 
         while (n > 0)   
         {
@@ -75,16 +69,81 @@ int main()
                     m++;     // счетчик +1
                 n--;     // последнюю букву убираем. первая убирается самим циклом
             }
-            
-            if (m == (kol /= 2))   // если счетчик = половине количества бувк
-            {
-                flag = true;
-            }
+        }
+
+        if (m == (kol /= 2))   // если счетчик = половине количества бувк
+        {
+            flag = true;
         }
         
         if (flag)
             break;
 
+    }
+
+    int count = 0;
+
+    int stock[100];    //счетчик согласных
+    for (int i = 0; i < 100; i++)
+    {
+        stock[i] = 0;
+    }
+
+    if (flag == false)  // если нет палиндрома
+    {
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = 0; words[i][j] != 0; j++)   // количество букв в слове
+            {
+                count++;
+            }
+
+            for (int j = 0; j < count; j++)     // количество согласных в слове
+            {
+                if ((words[i][j] != 'а') ||
+                    (words[i][j] != 'е') ||
+                    (words[i][j] != 'ё') ||
+                    (words[i][j] != 'и') ||
+                    (words[i][j] != 'о') ||
+                    (words[i][j] != 'у') ||
+                    (words[i][j] != 'ы') ||
+                    (words[i][j] != 'э') ||
+                    (words[i][j] != 'ю') ||
+                    (words[i][j] != 'я'))
+                {
+                    stock[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = i + 1; j < k-1; j++)
+            {
+                if (stock[i] < stock[j])
+                {
+                    int tmp = stock[i];
+                    stock[i] = stock[j];
+                    stock[j] = tmp;
+
+                    for (int h = 0; h < 100; h++)
+                    {
+                        int tmpp = words[i][h];
+                        words[i][h] = words[j][h];
+                        words[j][h] = tmpp;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                std::cout << words[i][j];
+            }
+            std::cout << std::endl;
+        }
     }
 
 
