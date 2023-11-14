5efce0867732bcab96948b294fd2a28c09e33d62
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2023-11-14 17:04:55 +0200

    Add files via upload

---
+++ laba8.3.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#define N 3000
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::ifstream file("input.txt");
+
+    char buff[100];
+
+    for (int i = 0; i < 100; i++)
+        buff[i] = 0;
+
+    char words[N][100];
+
+    int k = 0;
+
+    while (!file.eof())
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            words[k][i] = buff[i];
+            k++;
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            buff[i] = 0;
+        }
+    }
+
+
+    // сделать массив количества каждой буквы в слове
+    // найти пять наиболее часто встречающихся
+    // найти слова, где таких букв минимум 4
+
+}
+
