ed7d4c314713e16ae9d07d94e1e61c2e32f1d663
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2023-12-12 15:09:21 +0200

    Add files via upload

---
+++ laba11.1.cpp
@@ -0,0 +1,87 @@
+﻿
+
+#include <iostream>
+
+void read_matrix(int** matrixA, int n, int m)
+{
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            std::cin >> matrixA[i][j];
+}
+
+int sum_str(int** matrixA, int n, int m)
+{
+
+    int min = INT_MAX;
+    int str = -1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < m; ++j)
+            sum = sum + matrixA[i][j];
+
+        if (min > sum)
+        {
+            min = sum;
+            str = i;
+        }
+    }
+
+    return str;
+}
+
+void write_matrix(int** matrixA, int n, int m)
+{
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cout << matrixA[i][j] << " ";
+        }
+        std::cout << "" << std::endl;
+    }
+}
+
+
+int main()
+{
+    std::cout << "number 3. " << std::endl;
+    std::cout << " " << std::endl;
+
+
+    const int N_Max = 100;
+    const int M_Max = 100;
+
+    int n, m;
+
+    std::cout << " vvedite kolvo" << std::endl;
+    std::cin >> n >> m;
+
+    if (n > N_Max || m > M_Max)
+    {
+        std::cout << "error" << std::endl;
+        return -1;
+    }
+
+    int** matrixA = new int* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        matrixA[i] = new int[m];
+    }
+
+    read_matrix(matrixA, n, m);
+
+    int str = sum_str(matrixA, n, m);
+
+    for (int j = 0; j < m; ++j) {
+
+        matrixA[str][j] = 0;
+    }
+
+    write_matrix(matrixA, n, m);
+
+    delete[] matrixA[0];
+    delete[] matrixA;
+
+}
+
---
+++ lana11.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+
+void read_mas(int* mass, int n)
+{
+    for (int i = 0; i < n; ++i) {
+        std::cin >> mass[i];
+    }    
+}
+
+bool is_one(int* mass, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+
+        int x = mass[i];
+
+        while (x > 9)
+        {
+            x /= 10;
+        }
+
+        if (x == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+void sort_mas(int* mass, int n)
+{
+    for (int i = 0; i < n - 1; ++i)
+        for (int j = i + 1; j < n; ++j)
+            if (mass[i] > mass[j])
+            {
+                int tmp = mass[i];
+                mass[i] = mass[j];
+                mass[j] = tmp;
+            }
+}
+
+void write_mas(int* mass, int n)
+{
+    for (int i = 0; i < n; ++i) {
+        std::cout << mass[i] << std::endl;
+    }
+}
+
+
+int main()
+{
+    
+    setlocale(LC_ALL, "Russian");
+
+
+
+    std::cout << "number 1. " << std::endl;
+
+    const int N_MAX = 10000;
+
+    int n;
+    std::cout << "Введите количество чисел в последовательности" << std::endl;
+    std::cin >> n;
+
+    if (n > N_MAX) {
+        std::cout << "error" << std::endl;
+        exit(-1); // функция остановки. программа выведет -1.
+    }
+
+    int* mass = new int[n];
+
+    int i = 0;
+
+    read_mas(mass, n);
+
+    bool flag = is_one(mass, n);   
+
+
+    if (flag)
+    {
+        sort_mas(mass, n);
+    }
+
+    write_mas(mass, n);
+
+    delete[] mass;
+    
+}
